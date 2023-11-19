e0f58288f5d3c397a7e7a04ee0e974e5c137226f
Amir Eliz <amir.eliz155@gmail.com>
2023-11-19 20:08:23 +0200

    move to pnpm

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import Providers from './(main)/UI/Providers'
 import './styles/globals.scss'
 
 const notoHe = NotoHE({
-	subsets: ['latin'],
+	subsets: ['hebrew'],
 	weight: ['300', '400', '500', '600', '700', '800', '900'],
 	variable: '--font-noto',
 })
--- next.config.js
+++ next.config.js
@@ -1,6 +1,3 @@
 /** @type {import('next').NextConfig} */
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-	enabled: process.env.ANALYZE === 'true',
-})
 
-module.exports = withBundleAnalyzer({})
+module.exports = {}
